4b0b5178a4d92c5d6abfca752604f4dfe51f9447
Joao Mateus <jaomateus@gmail.com>
2023-06-18 23:33:38 +0100

    implement several search methods

---
+++ app/models/movie.rb
@@ -0,0 +1,14 @@
+class Movie < ApplicationRecord
+  belongs_to :director
+
+include PgSearch::Model
+
+pg_search_scope :global_search,
+  against: [ :title, :synopsis ],
+  associated_against: {
+    director: [ :first_name, :last_name ]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
+end
